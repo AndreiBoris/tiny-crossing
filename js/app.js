7d543fec024e74b4d3d73d4d2b0768e50a971cd0
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2015-10-29 09:15:57 -0400

    Chore: Add grunt skeleton

--- js/app.js
+++ js/app.js
@@ -25,11 +25,9 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 
 
 // This listens for key presses and sends the keys to your
